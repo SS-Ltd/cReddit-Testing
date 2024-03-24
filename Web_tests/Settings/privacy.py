cd978aa41b146a5bef2c34155d89ccce59c89fa7
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-03-24 18:27:53 +0200

    modified gitignore. Fixed the privacy subpage of the settings page to work as expected.

--- Web_tests/Settings/privacy.py
+++ Web_tests/Settings/privacy.py
@@ -5,39 +5,79 @@ This module contains all the necessary tests needed to test this subpage.
 
 import sys
 import os
-from helper_functions import locate_element
+from helper_functions import locate_element, check_popup_notification
 from constants import DELAY_TIME
-from my_imports import thread
+from my_imports import thread, WebDriver, By
 from write_to_files import write_to_all_files, report_fail
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def people(driver) -> None:
+def users_block(driver: WebDriver) -> None:
     '''
     This function tests blocking people functionality
     '''
-
     write_to_all_files("Testing Block People")
     input_field = locate_element(driver, by_id='safety-block-user-input')
     assert input_field is not None, report_fail("Block user input field not found")
     driver.execute_script("arguments[0].scrollIntoView();", input_field)
     input_field.clear()
     input_field.send_keys('Test User')
-    # This doesnt really do anything for now
-    # Will continue the tests later, probably on the actual reddit website
+    locate_element(driver, by_xpath='//*[@id="root"]/div/div[4]/div[3]/div/div[2]/button').click()
+    check_popup_notification(driver)
+    thread.sleep(DELAY_TIME)
+    # Check that the user has been added to the list of blocked users
+    users = driver.find_elements(By.XPATH, '//*[contains(text(), "Test User")]')
+    # check if users is not empty
+    assert users, report_fail("User not found in blocked users list")
+    for user in users:
+        # Go up two divs (to the grandparent)
+        grandparent = user.find_element(By.XPATH, '../..')
+        # Find a button within the grandparent's children
+        button = grandparent.find_element(By.XPATH, './/button')
+        assert button is not None, report_fail("Remove user from blocked list button not found")
+        # Click on the button
+        button.click()
+        check_popup_notification(driver)
+        thread.sleep(DELAY_TIME)
 
-def communities(driver) -> None:
+    # Check that the user has been removed from the list of blocked users
+    users = driver.find_elements(By.XPATH, '//*[contains(text(), "Test User")]')
+    assert not users, report_fail("User found in blocked users list")
+
+    write_to_all_files("Blocking users functionality works as expected")
+
+def communities_mute(driver) -> None:
     '''
     This function tests muting communities functionality
     '''
-
     write_to_all_files("Testing Block Communities")
     input_field = locate_element(driver, by_id='safety-mute-community-input')
     assert input_field is not None, report_fail("Mute community input field not found")
     driver.execute_script("arguments[0].scrollIntoView();", input_field)
     input_field.clear()
     input_field.send_keys('Test Community')
-    # This doesnt really do anything for now
-    # Will continue the tests later, probably on the actual reddit website
+    locate_element(driver, by_xpath='//*[@id="root"]/div/div[4]/div[3]/div/div[8]/button').click()
+    check_popup_notification(driver)
+    thread.sleep(DELAY_TIME)
+    # Check that the community has been added to the list of muted communities
+    communities = driver.find_elements(By.XPATH, '//*[contains(text(), "Test Community")]')
+    # check if communities is not empty
+    assert communities, report_fail("Community not found in muted communities list")
+    for community in communities:
+        # Go up two divs (to the grandparent)
+        grandparent = community.find_element(By.XPATH, '../..')
+        # Find a button within the grandparent's children
+        button = grandparent.find_element(By.XPATH, './/button')
+        assert button is not None, report_fail("Remove community from muted list button not found")
+        # Click on the button
+        button.click()
+        check_popup_notification(driver)
+        thread.sleep(DELAY_TIME)
+
+    # Check that the community has been removed from the list of muted communities
+    communities = driver.find_elements(By.XPATH, '//*[contains(text(), "Test Community")]')
+    assert not communities, report_fail("Community found in muted communities list")
+
+    write_to_all_files("Muting communities functionality works as expected")
 
 def privacy(driver) -> None:
     '''
@@ -47,10 +87,10 @@ def privacy(driver) -> None:
     write_to_all_files(
         "#################### Testing Safety & Privacy Subpage ####################")
 
-    people(driver)
-    thread.sleep(DELAY_TIME)
+    users_block(driver)
+    # thread.sleep(DELAY_TIME)
 
-    communities(driver)
+    communities_mute(driver)
 
     write_to_all_files(
         "#################### Safety & Privacy Subpage Test Completed ####################")
