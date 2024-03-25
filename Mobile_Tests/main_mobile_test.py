1d06b8cb8cb6f75af9c80269290640316b83ce27
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-03-25 11:09:27 +0200

    This is the main script that runs the mobile app and logs in.

--- Mobile_Tests/main_mobile_test.py
+++ Mobile_Tests/main_mobile_test.py
@@ -1,17 +1,55 @@
+'''
+This is the main test script for the mobile application.
+'''
+import time as thread
+from typing import Any, Dict
 from appium import webdriver
-import unittest
-import time
+from appium.options.common import AppiumOptions
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-class TestMobile(unittest.TestCase):
-    def setUp(self):
-        desired_caps = {
-            "platformName": "Android",
-            "platformVersion": "9",
-            "deviceName": "Android Emulator",
-            "app": "x",
-            "appPackage": "com.example.android.contactmanager",
-            "appActivity": ".ContactManager"
-        }
+cap: Dict[str, Any] = {
+    "platformName": "Android",
+    "automationName": "UiAutomator2",
+    "deviceName": "emulator-5556",
+    "appPackage": "com.example.reddit_clone",
+    "appActivity": "com.example.reddit_clone.MainActivity",
+    "language": "en",
+    "locale": "US"
+}
 
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
-        time.sleep(2)       
+URL = "http://localhost:4724"
+
+print("Starting the Appium Test")
+driver = webdriver.Remote(URL, options=AppiumOptions().load_capabilities(cap))
+# Check if the app is launched
+print(driver.current_activity)
+
+print("App launched")
+thread.sleep(2)
+
+# Click on the login button
+username = driver.find_element(
+    AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]')
+username.click()
+thread.sleep(2)
+# username.clear()
+username.send_keys("test")
+
+thread.sleep(5)
+
+password = driver.find_element(
+    AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]')
+password.click()
+thread.sleep(2)
+# password.clear()
+password.send_keys("test")
+
+login = driver.find_element(
+    AppiumBy.ACCESSIBILITY_ID, 'LOGIN')
+login.click()
+
+thread.sleep(10)
+# driver.quit()
