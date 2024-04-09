7578692960e0ecc6efc43156fa5ab4411d3c8487
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-09 19:29:52 +0200

    Mobile: Continued some implementations in the accounts settings page

--- Mobile_Tests/settings.py
+++ Mobile_Tests/settings.py
@@ -5,7 +5,13 @@ This module checks the functionalities of the settings page of the mobile applic
 from my_imports import thread
 from constants import DELAY_TIME
 from helper_functions import locate_element
-from Paths import SETTINGS_CONTENT_POLICY, SETTINGS_PRIVACY_POLICY, SETTINGS_USER_AGREEMENT, SETTINGS_HELP_CENTER, SETTINGS_VISIT_REDDIT_MOBILE
+# from Paths import SETTINGS_CONTENT_POLICY, SETTINGS_PRIVACY_POLICY, SETTINGS_USER_AGREEMENT
+# from Paths import SETTINGS_HELP_CENTER, SETTINGS_VISIT_REDDIT_MOBILE
+from Paths import UPDATE_EMAIL_USERNAME
+from Paths import SETTINGS_ACCOUNT
+from Paths import ACCOUNT_UPDATE_EMAIL
+from Settings.update_email import update_email
+
 
 def check_hyperlink(driver, hyperlink: str) -> None:
     '''
@@ -43,6 +49,20 @@ def check_hyperlink(driver, hyperlink: str) -> None:
     driver.swipe(100, 1000, 100, 100, 200)
     thread.sleep(DELAY_TIME)
 
+def account_settings(driver) -> None:
+    '''
+    This function checks the functionalities of the update email page of the mobile application.
+    '''
+
+    locate_element(driver, by_xpath=ACCOUNT_UPDATE_EMAIL).click()
+    thread.sleep(DELAY_TIME)
+    print("Update Email clicked")
+    # Check that you are in the update email page
+    temp = locate_element(driver, by_accessibility_id=UPDATE_EMAIL_USERNAME)
+    assert temp is not None, "Update Email page not found"
+    update_email(driver)
+
+
 
 def settings(driver) -> None:
     '''
@@ -54,13 +74,23 @@ def settings(driver) -> None:
     assert temp is not None, "Settings page not found"
     print("Settings page found")
 
-    driver.swipe(100, 1000, 100, 100, 400)
-
+    # Check the functionalities of the Account settings
+    # Go to the Account Settings page
+    locate_element(driver, by_xpath=SETTINGS_ACCOUNT).click()
     thread.sleep(DELAY_TIME)
+    print("Account Settings clicked")
+
+    # Check the functionalities of the Account settings page
+    account_settings(driver)
+    print("Account settings functionalities checked")
+
+    # driver.swipe(100, 1000, 100, 100, 400)
+
+    # thread.sleep(DELAY_TIME)
 
     # Check the functionalities of the hyperlinks
-    check_hyperlink(driver, SETTINGS_CONTENT_POLICY)
-    check_hyperlink(driver, SETTINGS_PRIVACY_POLICY)
-    check_hyperlink(driver, SETTINGS_USER_AGREEMENT)
-    check_hyperlink(driver, SETTINGS_HELP_CENTER)
-    check_hyperlink(driver, SETTINGS_VISIT_REDDIT_MOBILE)
+    # check_hyperlink(driver, SETTINGS_CONTENT_POLICY)
+    # check_hyperlink(driver, SETTINGS_PRIVACY_POLICY)
+    # check_hyperlink(driver, SETTINGS_USER_AGREEMENT)
+    # check_hyperlink(driver, SETTINGS_HELP_CENTER)
+    # check_hyperlink(driver, SETTINGS_VISIT_REDDIT_MOBILE)
