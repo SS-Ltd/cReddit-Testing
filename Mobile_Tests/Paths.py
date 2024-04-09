d62176d5d4598b8ff44b050bd67690bcd1f632e0
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-09 14:25:01 +0200

    Refactored the main test file for mobile tests

---
+++ Mobile_Tests/Paths.py
@@ -0,0 +1,12 @@
+'''
+This module contains all the xPaths and accessibility IDs for the mobile application.
+'''
+
+# Starting page for the mobile application
+START_USERNAME = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]'
+START_PASSWORD = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]'
+START_LOGIN = 'Continue'
+
+# Home page
+# Bottom tabs
+HOME_PAGE_TABS_HOME = 'Home\nTab 1 of 5'
