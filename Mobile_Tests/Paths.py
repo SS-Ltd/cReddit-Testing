7ad8fade0de8d71cfc74049f6fa63086e1351e75
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-09 19:29:52 +0200

    Mobile: Continued some implementations in the accounts settings page

--- Mobile_Tests/Paths.py
+++ Mobile_Tests/Paths.py
@@ -25,8 +25,28 @@ PROFILE_SETTINGS = 'Settings'
 HOME_PAGE_TABS_HOME = 'Home\nTab 1 of 5'
 
 # Settings page
+SETTINGS_ACCOUNT = '//android.widget.Button[@content-desc="Account Settings u/Claudine"]'
 SETTINGS_CONTENT_POLICY = 'Content Policy'
 SETTINGS_PRIVACY_POLICY = 'Privacy Policy'
 SETTINGS_USER_AGREEMENT = 'User Agreement'
 SETTINGS_HELP_CENTER = 'Help Center'
 SETTINGS_VISIT_REDDIT_MOBILE = 'Visit r/redditmobile'
+
+# Account settings page
+ACCOUNT_UPDATE_EMAIL = '//android.widget.Button[@content-desc="Update email address\nGloria.Grady@hotmail.com"]'
+
+# Update Email page
+UPDATE_EMAIL_USERNAME = 'u/username'
+UPDATE_EMAIL_EMAIL = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]'
+UPDATE_EMAIL_PASSWORD = '//android.widget.ScrollView/android.widget.EditText[2]'
+UPDATE_EMAIL_SAVE = 'Save'
+UPDATE_EMAIL_CANCEL = 'Cancel'
+UPDATE_EMAIL_FORGOT_PASSWORD = 'Forgot password?'
+UPDATE_EMAIL_ERROR_EMAIL_EMPTY = 'Please enter an email address.'
+UPDATE_EMAIL_ERROR_EMAIL_INVALID = 'Please enter a valid email address.'
+UPDATE_EMAIL_ERROR_PASSWORD_EMPTY = 'Please enter your password'
+UPDATE_EMAIL_ERROR_PASSWORD_INVALID = 'Password must be at least 8 characters long'
+
+# Forgot Password page
+FORGOT_PASSWORD_WINDOW = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View'
+FORGOT_PASSWORD_HAVING_TROUBLE = 'Having Trouble?'
