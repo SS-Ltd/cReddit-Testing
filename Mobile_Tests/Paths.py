13afb52814ff8fa3451243e8378612780312003f
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-09 16:21:44 +0200

    Mobile: Added checks for hyperlinks from settings page

--- Mobile_Tests/Paths.py
+++ Mobile_Tests/Paths.py
@@ -8,9 +8,19 @@ START_PASSWORD = '//android.widget.FrameLayout[@resource-id="android:id/content"
 START_LOGIN = 'Continue'
 
 # Home page
+
 # Profile Icon
 HOME_PAGE_PROFILE_ICON = '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[4]'
+
 # Within Profile Window
 PROFILE_SETTINGS = 'Settings'
+
 # Bottom tabs
 HOME_PAGE_TABS_HOME = 'Home\nTab 1 of 5'
+
+# Settings page
+SETTINGS_CONTENT_POLICY = 'Content Policy'
+SETTINGS_PRIVACY_POLICY = 'Privacy Policy'
+SETTINGS_USER_AGREEMENT = 'User Agreement'
+SETTINGS_HELP_CENTER = 'Help Center'
+SETTINGS_VISIT_REDDIT_MOBILE = 'Visit r/redditmobile'
