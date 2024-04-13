dc70b18e6126a1f5ab618c88e11b364c1b038515
Hussein Elhawary <husseinelhawary@gmail.com>
2024-04-13 22:30:24 +0200

    Refactor login module and implement forgot password functionality

--- Mobile_Tests/main_mobile_test.py
+++ Mobile_Tests/main_mobile_test.py
@@ -29,7 +29,7 @@ print(driver.current_activity)
 print("App launched")
 
 # Test the login functionality
-
+login(driver)
 # Click on the login button
 username = locate_element(driver, by_xpath=START_USERNAME)
 username.click()
@@ -43,8 +43,8 @@ thread.sleep(2)
 # password.clear()
 password.send_keys("1")
 
-login = locate_element(driver, by_accessibility_id=START_LOGIN)
-login.click()
+login_element = locate_element(driver, by_accessibility_id=START_LOGIN)
+login_element.click()
 
 # Verify that the login was successful
 # Check the bottom tabs, one of them is an enough indication that the login was successful
