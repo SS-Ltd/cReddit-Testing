8a6b0566b3d4828c824375d1dea1694cac8ffc93
Hussein Elhawary <husseinelhawary@gmail.com>
2024-04-15 21:27:16 +0200

    Refactor login module and implement forgot password functionality

--- Mobile_Tests/constants.py
+++ Mobile_Tests/constants.py
@@ -9,5 +9,6 @@ DELAY_TIME = 5
 SEE_TIME = 2
 USERNAME = str(os.getenv("USERNAME1"))
 EMAIL = str(os.getenv("EMAIL"))
+#PASSWORD = str(os.getenv("PASSWORD"))
 PASSWORD_CREDDIT = str(os.getenv("PASSWORD_CREDDIT"))
 EMAIL_SIGNUP = str(os.getenv("EMAIL_SIGNUP"))
--- Web_tests/constants.py
+++ Web_tests/constants.py
@@ -12,12 +12,13 @@ LOG_FILE_PATH = "../Web_Text_Files/log_file.txt"
 RUN_TIME_STATISTICS_FILE_PATH = "../Web_Text_Files/run_time_statistics.txt"
 FIREFOX_DRIVER_PATH = "C:/geckodriver.exe"
 FIREFOX_BINARY_PATH = "C:/Program Files/Mozilla Firefox/firefox.exe"
-DELAY_TIME = 4
+DELAY_TIME = 10
 SEE_TIME = 0
-#SITE_NAME = "https://creddit.tech?"
-SITE_NAME = "http://localhost:5173/"
+SITE_NAME = "https://creddit.tech/"
+#SITE_NAME = "http://localhost:5173/"
 USERNAME = str(os.getenv("USERNAME1"))
 EMAIL = str(os.getenv("EMAIL"))
 PASSWORD = str(os.getenv("PASSWORD"))
+CREDDIT_PASSWORD = str(os.getenv("PASSWORD_CREDDIT"))
 EMAIL_SIGNUP = str(os.getenv("EMAIL_SIGNUP"))
 # http://creddit.tech
