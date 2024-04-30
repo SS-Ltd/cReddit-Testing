a610fd6d4360866969ce39a89a59eec238aefad4
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-30 03:02:35 +0300

    Some UserCard Functionalities

--- Web_tests/paths.py
+++ Web_tests/paths.py
@@ -30,10 +30,17 @@ RIGHT_SIDE_BAR_COMMUNITY_NAME = "_community_recent"
 RIGHT_SIDE_BAR_POST_TITLE = "_post_header"
 
 # Profile Page
-PROFILE_USERNAME = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[1]/div[1]/div/p'  # xpath
-PROFILE_MAINFEED_CATEGORY_DROPDOWN = 'mainfeed_category_dropdown'                   # id
-PROFILE_MAINFEED_CATEGORY_HOT = 'mainfeed_category_hot'                             # id
-PROFILE_MAINFEED_CATEGORY_NEW = 'mainfeed_category_new'                             # id
-PROFILE_MAINFEED_CATEGORY_TOP = 'mainfeed_category_top'                             # id
-PROFILE_FEED = 'profile-buttons-row'                                                # id
-PROFILE_FIRST_COMMENT = '//*[@id="mainfeed"]/div[3]/div[1]/div/p[1]'                # xpath
+PROFILE_USERNAME = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[1]/div[1]/div/p'                                  # xpath
+PROFILE_MAINFEED_CATEGORY_DROPDOWN = 'mainfeed_category_dropdown'                                                   # id
+PROFILE_MAINFEED_CATEGORY_HOT = 'mainfeed_category_hot'                                                             # id
+PROFILE_MAINFEED_CATEGORY_NEW = 'mainfeed_category_new'                                                             # id
+PROFILE_MAINFEED_CATEGORY_TOP = 'mainfeed_category_top'                                                             # id
+PROFILE_FEED = 'profile-buttons-row'                                                                                # id
+PROFILE_FIRST_COMMENT = '//*[@id="mainfeed"]/div[3]/div[1]/div/p[1]'                                                # xpath
+PROFILE_FOLLOW = 'follow-btn-usercard'                                                                              # id
+PROFILE_CHAT = 'chat-btn-usercard'                                                                                  # id
+PROFILE_USER_DROPDOWN = 'users-drop-menu-down'                                                                      # id
+PROFILE_USER_DROPDOWN_SHARE = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[2]/div/div/div[1]/div/div[2]/div[1]'   # xpath
+PROFILE_USER_DROPDOWN_SEND = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[2]/div/div/div[1]/div/div[2]/div[2]'    # xpath
+PROFILE_USER_DROPDOWN_BLOCK = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[2]/div/div/div[1]/div/div[2]/div[3]'   # xpath
+PROFILE_USER_DROPDOWN_REPORT = '//*[@id="root"]/div/div[3]/div/div[3]/div/div[2]/div/div/div[1]/div/div[2]/div[4]'  # xpath
--- Web_tests/profile.py
+++ Web_tests/profile.py
@@ -8,6 +8,8 @@ from helper_functions import locate_element
 from paths import PROFILE_USERNAME, PROFILE_FEED, PROFILE_FIRST_COMMENT
 from paths import PROFILE_MAINFEED_CATEGORY_DROPDOWN
 from paths import PROFILE_MAINFEED_CATEGORY_NEW, PROFILE_MAINFEED_CATEGORY_HOT, PROFILE_MAINFEED_CATEGORY_TOP
+from paths import PROFILE_FOLLOW, PROFILE_CHAT, PROFILE_USER_DROPDOWN
+from paths import PROFILE_USER_DROPDOWN_SHARE, PROFILE_USER_DROPDOWN_SEND, PROFILE_USER_DROPDOWN_BLOCK, PROFILE_USER_DROPDOWN_REPORT
 
 def sort_feed(driver, feed: str) -> None:
     '''
@@ -36,16 +38,86 @@ def sort_feed(driver, feed: str) -> None:
     print(dropdown_p.text)
     assert dropdown_p.text == feed, "The dropdown is not sorted by " + feed
 
-
-def profile(driver) -> None:
+def test_usercard(driver) -> None:
     '''
-    This function is supposed to test the Profile page of the website.
+    This function is supposed to test the UserCard of the Profile page.
     '''
 
-    # Go to a random user's profile
-    driver.get(SITE_NAME + "user/Taya_Shanahan38/")
+    # Locate the follow button and click on it
+    follow_button = locate_element(driver, by_id=PROFILE_FOLLOW)
+    assert follow_button is not None, "The follow button is not present"
+    follow_button.click()
+    # thread.sleep(DELAY_TIME)
+    # Check the p tag inside the follow_button
+    # assert locate_element(follow_button, by_xpath=".//p").text == "Unfollow", "The follow button is not working"
+
     thread.sleep(DELAY_TIME)
 
+    # Click on the follow button again
+    # follow_button.click()
+    # thread.sleep(DELAY_TIME)
+    # Check that the button text is now "Follow"
+    # assert locate_element(follow_button, by_xpath=".//p").text == "Follow", "The follow button is not working"
+
+    # Locate the chat button and click on it
+    chat_button = locate_element(driver, by_id=PROFILE_CHAT)
+    assert chat_button is not None, "The chat button is not present"
+    chat_button.click()
+    thread.sleep(DELAY_TIME)
+    # Check that the url contains the correct path
+    assert "chat" in driver.current_url, "The Chat URL is incorrect"
+    # Go back to the profile page
+    driver.back()
+    thread.sleep(DELAY_TIME)
+
+    # Locate the user dropdown button and click on it
+    user_dropdown = locate_element(driver, by_id=PROFILE_USER_DROPDOWN)
+    assert user_dropdown is not None, "The user dropdown button is not present"
+    user_dropdown.click()
+    thread.sleep(1)
+
+    # Locate the share button and click on it
+    share_button = locate_element(driver, by_xpath=PROFILE_USER_DROPDOWN_SHARE)
+    assert share_button is not None, "The share button is not present"
+    share_button.click()
+    # NOT IMPLEMENTED YET
+    print("Share Clicked")
+
+    # Locate the send button and click on it
+    send_button = locate_element(driver, by_xpath=PROFILE_USER_DROPDOWN_SEND)
+    assert send_button is not None, "The send button is not present"
+    send_button.click()
+    # NOT IMPLEMENTED YET
+    print("Send Clicked")
+
+    # Locate the block button and click on it
+    block_button = locate_element(driver, by_xpath=PROFILE_USER_DROPDOWN_BLOCK)
+    assert block_button is not None, "The block button is not present"
+    block_button.click()
+    thread.sleep(DELAY_TIME)
+    # Check that an unblock button has appeared
+    follow_button = locate_element(driver, by_id=PROFILE_FOLLOW)
+    assert locate_element(follow_button, by_xpath=".//p").text == "UnBlock", "The block button is not working"
+    # Click on the unblock button
+    follow_button.click()
+
+    # Locate the user dropdown button and click on it
+    user_dropdown = locate_element(driver, by_id=PROFILE_USER_DROPDOWN)
+    assert user_dropdown is not None, "The user dropdown button is not present"
+    user_dropdown.click()
+    thread.sleep(1)
+
+    # Locate the report button and click on it
+    report_button = locate_element(driver, by_xpath=PROFILE_USER_DROPDOWN_REPORT)
+    assert report_button is not None, "The report button is not present"
+    report_button.click()
+    # NOT IMPLEMENTED YET
+    print("Report Clicked")
+
+def test_feed(driver) -> None:
+    '''
+    This function tests the feed of the profile page
+    '''
     # Locate the username and check that it contains the correct username
     username = locate_element(driver, by_xpath=PROFILE_USERNAME)
     assert username.text == "u/Taya_Shanahan38", "The username is incorrect"
@@ -86,3 +158,18 @@ def profile(driver) -> None:
     assert "Taya_Shanahan38" in comment_p.text, "The comment is incorrect"
     thread.sleep(DELAY_TIME)
 
+
+def profile(driver) -> None:
+    '''
+    This function is supposed to test the Profile page of the website.
+    '''
+
+    # Go to a random user's profile
+    driver.get(SITE_NAME + "user/Taya_Shanahan38/")
+    thread.sleep(DELAY_TIME)
+
+    # Test1: Test the feed
+    # test_feed(driver)
+
+    # Test2: Test the user card
+    test_usercard(driver)
