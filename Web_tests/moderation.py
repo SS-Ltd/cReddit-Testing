0752af8e4a3e755aea6bf627f96acb95a719c6c2
Hussein Elhawary <husseinelhawary@gmail.com>
2024-05-10 15:41:57 +0300

    feat: Add moderation page tests file

---
+++ Web_tests/moderation.py
@@ -0,0 +1,11 @@
+'''
+This file contains the tests for the moderation page.
+'''
+
+from my_imports import thread, By, Keys
+from write_to_files import write_to_file
+from helper_functions import locate_element
+from constants import DELAY_TIME
+
+
+def moderation(driver):
